34a620ea4a4c5ebb72ce6b864eb9456bd56c26a1
Sandré dos Santos Pereira <sandre.pereira@stone.com.br>
2021-09-29 10:20:52 -0300

    Add project files.

---
+++ Worker.App.FluentValidation/Worker.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Worker.App.FluentValidation
+{
+    class Worker
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
